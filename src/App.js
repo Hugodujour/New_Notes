2b055198c2879f81267aa8bed1340e847dcc4c1f
Hugo <hugogreg@live.fr>
2023-01-31 18:09:33 +0100

    button delete workin'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import {useState} from 'react'
+import uuid from 'react-uuid'
 import './App.css';
 import Main from './Main';
 import Sidebar from './Sidebar';
 
 function App() {
+
+  const [notes, setNotes] = useState([])
+
+
+  const onAddNote = () => {
+    const newNote = {
+
+      id: uuid(),
+
+      title: 'Untitled Note',
+
+      body: '',
+
+      lastModified: Date.now(),
+
+    }
+
+    
+
+    setNotes([newNote, ...notes])
+
+  }
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete))
+  }
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar notes={notes} onAddNote={onAddNote} onDeleteNote={onDeleteNote}/>
       <Main />
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,6 @@
-function Sidebar() {
+
+
+function Sidebar({ notes, onAddNote, onDeleteNote }) {
 
 
   return (
@@ -6,22 +8,30 @@ function Sidebar() {
 
       <div className="app-sidebar-header">
         <h1>Notes</h1>
-        <button>Add</button>
+        <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
+
+        {notes.map((note) => (
 
         <div className="app-sidebar-note">
 
           <div className="sidebar-note-title">
-            <strong>TITLE</strong>
-            <button>Delete</button>
+            <strong>{note.title}</strong>
+            <button onClick={() => onDeleteNote(note.id)}>Delete</button>
           </div>
 
-          <p>Note preview</p>
+          <p>{note.body && note.body.substr(0, 100) + "..."}</p>
 
-          <small className="note-meta">Last modified [date]</small>
+          <small className="note-meta">
+
+            Last modified {new Date (note.lastModified).toLocaleDateString("en-GB", {
+              hour: "2-digit",
+              minute: "2-digit"
+            })}</small>
 
         </div>
+        ))}
 
       </div>
     </div>
